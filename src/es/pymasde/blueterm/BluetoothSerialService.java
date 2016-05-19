03033b6d57b98d7fd507c78b97868337b73fd62d
Semyon Maryasin <simeon@maryasin.name>
2016-05-20 02:38:18 +0300

    Do stop accept threads

--- src/es/pymasde/blueterm/BluetoothSerialService.java
+++ src/es/pymasde/blueterm/BluetoothSerialService.java
@@ -209,7 +209,6 @@ public class BluetoothSerialService {
     public synchronized void stop() {
         if (D) Log.d(TAG, "stop");
 
-
         if (mConnectThread != null) {
             mConnectThread.cancel();
             mConnectThread = null;
@@ -218,6 +217,16 @@ public class BluetoothSerialService {
         if (mConnectedThread != null) {
             mConnectedThread.cancel();
             mConnectedThread = null;
+        }
+
+        if (mSecureAcceptThread != null) {
+            mSecureAcceptThread.cancel();
+            mSecureAcceptThread = null;
+        }
+
+        if (mInsecureAcceptThread != null) {
+            mInsecureAcceptThread.cancel();
+            mInsecureAcceptThread = null;
         }
 
         setState(STATE_NONE);
